2579d6dda79b7fadaa6ab9dbb896f8c6999e1188
Elli Beres <elliberes@proton.me>
2023-04-22 02:12:14 -0500

    Update install_scheme.c

--- install_scheme.c
+++ install_scheme.c
@@ -5,11 +5,11 @@
 #include <ctype.h>
 
 #define USAGE_MESSAGE		"Usage: %s <USERNAME> <RELATIVE_PATH_TO_SCHEME> [SCHEME_FILENAME=RELATIVE_PATH_TO_SCHEME]\n"
-#define OOM_MESSAGE			"%s: error: out of memory\n"
+#define OOM_MESSAGE		"%s: error: out of memory\n"
 #define LENGTH_MESSAGE		"%s: error: total file path exceeds 100 characters; please limit PATH_TO_COLOR_SCHEME to %lu characters\n"
 #define FILE_OPEN_MESSAGE	"%s: error: failed to open file: %s\n"
 
-#define PATH_LENGTH			123
+#define PATH_LENGTH		123
 
 int main(int argc, char* argv[]) {
 
@@ -21,8 +21,8 @@ int main(int argc, char* argv[]) {
 
 	// check if --help
 	if (argc >= 3 && strcmp(argv[1], "--help") == 0) {
-			fprintf(stderr, USAGE_MESSAGE, argv[0]);
-			return 2;
+		fprintf(stderr, USAGE_MESSAGE, argv[0]);
+		return 2;
 	}
 
 	// initialize paths
@@ -41,8 +41,8 @@ int main(int argc, char* argv[]) {
 
 	// check siz of file path
 	if (strlen(instl_dir) + strlen(instl_file) > PATH_LENGTH -1) {
-			fprintf(stderr, LENGTH_MESSAGE, argv[0], PATH_LENGTH - strlen(instl_dir) - 1);
-			return 1;
+		fprintf(stderr, LENGTH_MESSAGE, argv[0], PATH_LENGTH - strlen(instl_dir) - 1);
+		return 1;
 	}
 
 	FILE* in_file = fopen(clr_schm, "r");
@@ -56,10 +56,10 @@ int main(int argc, char* argv[]) {
 	printf("installing to: %s\n", instl_dir);
 
 	FILE* out_file = fopen(instl_dir, "w");
-		if (!out_file) {
-			fprintf(stderr, FILE_OPEN_MESSAGE, argv[0], instl_dir);
-			return 3;
-		}
+	if (!out_file) {
+		fprintf(stderr, FILE_OPEN_MESSAGE, argv[0], instl_dir);
+		return 3;
+	}
 
 
 	char nxt_char;
